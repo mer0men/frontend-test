d841fc1ff4dbb30548555c63bab2a7d1d154d2d4
unknown <meromen@github.com>
2019-10-07 23:44:41 +1000

    Add Comment list

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,61 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div#app
+    router-view
 </template>
 
 <style lang="scss">
+body, html {
+  margin: 0;
+}
+
+p {
+  margin: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background: #F8F8F8;
+  height: calc(100vh - 64px);
+  overflow: hidden;
+  padding: 32px 16px;
+  font-size: 14px;
+  line-height: 16px;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+.header {
+  font-weight: bold;
+}
+
+.orange {
+  color: #F56A3F;
+}
+
+.bordered-item {
+  border: 1px solid #D9D9D9;
+  border-radius: 8px;
+}
+
+.btn {
+  padding: 16px;
+  width: 100%;
+  font-weight: bold;
+  font-family: 'Roboto', sans-serif;
+  margin: 24px 0;
+
+  &:focus {
+    outline: none;
   }
+}
+
+.btn-light {
+  color: #000000;
+  background: #FFFFFF;
+}
+
+.btn-dark {
+  color: #FFFFFF;
+  background: #C4C4C4;
 }
 </style>
---
+++ src/views/CommentsList.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+div.comments-list
+  p.orange.header Отзывы магазина
+  button.btn.btn-light.bordered-item Написать отзыв
+  div.list
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+    comment-prev( v-bind:comment="comment")
+</template>
+
+<script>
+import CommentPrev from '@/components/CommentPrev'
+
+export default {
+  components: {
+    CommentPrev
+  },
+  data () {
+    return {
+      comment: {
+        rating: 4
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.comments-list {
+  position: relative;
+
+  .list {
+    height: 65vh;
+    overflow-y: scroll;
+  }
+}
+</style>
