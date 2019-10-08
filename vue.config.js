aff744e22ae1725d7a93dbb4a4b3b555828b009c
unknown <meromen@github.com>
2019-10-08 16:44:12 +1000

    Deploy build in git pages

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/frontend-test/'
+    : '/',
+  productionSourceMap: false,
+  filenameHashing: false,
+  outputDir: 'docs',
+  lintOnSave: process.env.NODE_ENV !== 'production'
+}
